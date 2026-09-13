fe8d742e8c6e92a80bf4535a1f5e36c9cd821185
nikgorbachev <gorbachev.nm@gmail.com>
2026-09-13 18:13:04 +0200

    slightly reordered initial screen

--- shared/src/commonMain/kotlin/com/example/algorithmic_pasticceria/App.kt
+++ shared/src/commonMain/kotlin/com/example/algorithmic_pasticceria/App.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -14,10 +15,9 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 
-import algorithmic_pasticceria.shared.generated.resources.Res
-import algorithmic_pasticceria.shared.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
@@ -26,23 +26,18 @@ fun App() {
         var showContent by remember { mutableStateOf(false) }
         Column(
             modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
+                .fillMaxSize()
+                .safeContentPadding(),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+                Text("Enter the Pasticceria! 🥐")
             }
             AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
+                Text(
+                    text = "Welcome at the Algorithmic Pasticceria!",
+                    modifier = Modifier.padding(16.dp)
+                )
             }
         }
     }
